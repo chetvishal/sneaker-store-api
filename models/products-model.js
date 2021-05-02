08c0ab8f37ca3b039f067d168c2a07f81688571d
chetvishal <vishal1982c@gmail.com>
2021-05-02 21:04:31 +0530

    refactored code

--- models/products-model.js
+++ models/products-model.js
@@ -1,15 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(
-//     'mongodb+srv://admin:babubhai123@neog-cluster.yuntr.mongodb.net/ecommerce',
-//     {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     }
-// )
-//     .then(() => console.log('Successfully connected to mongoDB'))
-//     .catch(err => console.log("Error connecting to mongoDB: ", err))
-
 const ProductSchema = new mongoose.Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
--- models/wishlist-model.js
+++ models/wishlist-model.js
@@ -1,15 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(
-//     'mongodb+srv://admin:babubhai123@neog-cluster.yuntr.mongodb.net/ecommerce',
-//     {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     }
-// )
-//     .then(() => console.log('Successfully connected to mongoDB'))
-//     .catch(err => console.log("Error connecting to mongoDB: ", err))
-
 const WishlistSchema = new mongoose.Schema({
     _id: { type: String, required: true },
 });
